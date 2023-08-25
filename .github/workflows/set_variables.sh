1a06f63f95b0983958fbbd96dc6b6af412192130
Jean-Baptiste VESLIN <19856429+thebaptiste@users.noreply.github.com>
2023-08-25 09:58:00 +0200

    build: fix ci (#75)

--- .github/workflows/set_variables.sh
+++ .github/workflows/set_variables.sh
@@ -13,7 +13,7 @@ case "${GITHUB_EVENT_NAME}" in
     repository_dispatch)
         B=${PAYLOAD_BRANCH};;
     workflow_dispatch)
-        B=$(WORKFLOW_BRANCH);;
+        B=${WORKFLOW_BRANCH};;
     pull_request)
         case "${GITHUB_BASE_REF}" in
             master | integration | experimental* | release_* | ci* | pci*)
@@ -32,7 +32,6 @@ case "${GITHUB_EVENT_NAME}" in
         esac;;
 esac
 
-echo "B" "${B}"
 if [ "${GITHUB_EVENT_NAME}" != "repository_dispatch" ] && [ "${GITHUB_EVENT_NAME}" != "workflow_dispatch" ]; then
     case "${GITHUB_REF}" in
         refs/tags/v*)
